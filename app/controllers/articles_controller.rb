110d63550d67c3086c0127a3b0a9d7321543afa6
florian fremin <florian.fremin33@gmail.com>
2017-03-20 17:51:08 +0100

    navbar + controller

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,60 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :search_article, only: [:index]
+
+  def index
+
+  end
+
+  def show
+  end
+
+  def new
+    @categories = Category.all
+    @article = Article.new
+  end
+
+  def create
+    @categories = Category.all
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description, :picture, :category_id, :preface)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def search_article
+    if params[:search].blank?
+      @articles = Article.all
+    else
+      @articles = Article.search_by_title(params[:search])
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  resources :articles
+  resources :categories, only: [:index, :show]
   mount Attachinary::Engine => "/attachinary"
 end
