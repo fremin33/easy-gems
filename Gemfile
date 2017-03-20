639ee3b0ec5e29ad33e82fad03ab2a06658d5b0b
florian fremin <florian.fremin33@gmail.com>
2017-03-20 18:51:03 +0100

    add pg_search

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem "cloudinary", "1.1.0"
 gem "attachinary", github: "assembler/attachinary"
 gem "jquery-fileupload-rails"
 gem 'coffee-rails'
+gem 'pg_search'
 
 group :development, :test do
   gem 'binding_of_caller'
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,10 @@
 class Article < ApplicationRecord
   belongs_to :category
   has_attachment  :picture
+  include PgSearch
+  pg_search_scope  :search_by_title,
+                    :against => :title,
+                    :using => {
+                    :tsearch => { :prefix => true, :negation => true, :any_word => true, :dictionary => "english" },
+                  }
 end
