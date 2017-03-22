46222d4f8bbc552b3cd8f65c08eb335ec19fde93
florian fremin <florian.fremin33@gmail.com>
2017-03-22 17:15:52 +0100

    add autocompletion fix

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,7 +6,6 @@ class Article < ApplicationRecord
   pg_search_scope  :search_by_title,
                     :against => :title,
                     :using => {
-                      :tsearch => { :prefix => true, :negation => true, :any_word => true, :dictionary => "english" },
                       :trigram => {
                         :threshold => 0.3
                       }
